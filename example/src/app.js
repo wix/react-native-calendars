905646624999cd5ec637be4836fc719526b6ba6c
Tautvilas <tautvilas@wix.com>
2017-05-14 13:40:50 +0300

    feat: support localization

--- example/src/app.js
+++ example/src/app.js
@@ -2,6 +2,19 @@ import {Navigation} from 'react-native-navigation';
 import {registerScreens} from './screens';
 registerScreens();
 
+/*
+import {LocaleConfig} from 'react-native-calendars';
+
+LocaleConfig.locales['fr'] = {
+  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+  dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+  dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.']
+};
+
+LocaleConfig.defaultLocale = 'fr';
+*/
+
 Navigation.startSingleScreenApp({
   screen: {
     screen: 'Menu',
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Calendar from './calendar';
 import CalendarList from './calendar-list';
 import Agenda from './agenda';
+import XDate from 'xdate';
+
+const LocaleConfig = XDate;
 
 module.exports = {
   Calendar,
   CalendarList,
-  Agenda
+  Agenda,
+  LocaleConfig
 };
