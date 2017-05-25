699c706997a7afecfc50d65a4ef1e95f18525133
Kurtis Kemple <kurtiskemple@gmail.com>
2017-05-25 11:30:20 -0400

    fix(custom arrows): remove unneeded React.cloneElement call

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -1,13 +1,8 @@
-import React, {Component} from 'react';
-import {ActivityIndicator} from 'react-native';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Image
-} from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import styleConstructor from './style';
-import {weekDayNames} from '../../dateutils';
+import { weekDayNames } from '../../dateutils';
 
 class CalendarHeader extends Component {
   constructor(props) {
@@ -26,7 +21,10 @@ class CalendarHeader extends Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    if (nextProps.month.toString('yyyy MM') !== this.props.month.toString('yyyy MM')) {
+    if (
+      nextProps.month.toString('yyyy MM') !==
+      this.props.month.toString('yyyy MM')
+    ) {
       return true;
     }
     if (nextProps.showIndicator !== this.props.showIndicator) {
@@ -36,41 +34,52 @@ class CalendarHeader extends Component {
   }
 
   render() {
-    let leftArrow = (<View/>);
-    let rightArrow = (<View/>);
+    let leftArrow = <View />;
+    let rightArrow = <View />;
     let weekDaysNames = weekDayNames(this.props.firstDay);
     if (!this.props.hideArrows) {
       leftArrow = (
-        <TouchableOpacity onPress={this.substractMonth} style={this.style.arrow}>
+        <TouchableOpacity
+          onPress={this.substractMonth}
+          style={this.style.arrow}
+        >
           {this.props.renderArrow
-            ? React.cloneElement(this.props.renderArrow('left'))
-            : <Image source={require('../img/previous.png')} style={this.style.arrowImage} />}
+            ? this.props.renderArrow('left')
+            : <Image
+                source={require('../img/previous.png')}
+                style={this.style.arrowImage}
+              />}
         </TouchableOpacity>
       );
       rightArrow = (
         <TouchableOpacity onPress={this.addMonth} style={this.style.arrow}>
           {this.props.renderArrow
-            ? React.cloneElement(this.props.renderArrow('right'))
-            : <Image source={require('../img/next.png')} style={this.style.arrowImage} />}
+            ? this.props.renderArrow('right')
+            : <Image
+                source={require('../img/next.png')}
+                style={this.style.arrowImage}
+              />}
         </TouchableOpacity>
       );
     }
     let indicator;
     if (this.props.showIndicator) {
-      indicator = (<ActivityIndicator/>);
+      indicator = <ActivityIndicator />;
     }
     return (
       <View>
         <View style={this.style.header}>
           {leftArrow}
-          <View style={{flexDirection: 'row'}}>
-            <Text style={this.style.monthText}>{this.props.month.toString('MMMM yyyy')}</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={this.style.monthText}>
+              {this.props.month.toString('MMMM yyyy')}
+            </Text>
             {indicator}
           </View>
           {rightArrow}
         </View>
         <View style={this.style.week}>
-          {weekDaysNames.map((day) => (
+          {weekDaysNames.map(day => (
             <Text key={day} style={this.style.dayHeader}>{day}</Text>
           ))}
         </View>
