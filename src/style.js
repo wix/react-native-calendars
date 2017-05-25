8b7c6a39eaf124cdabe1f0b80079f2757e713e28
Guy Carmeli <guyc@wix.com>
2017-05-25 16:37:50 +0300

    Change default text color to dark10 from ui-lib

--- src/style.js
+++ src/style.js
@@ -1,3 +1,5 @@
+import {Colors} from 'react-native-ui-lib';
+
 export const foregroundColor = '#ffffff';
 export const backgroundColor = '#f4f4f4';
 export const separatorColor = '#e8e9ec';
@@ -6,7 +8,7 @@ export const processedColor = '#a7e0a3';
 export const processingColor = '#ffce5c';
 export const failedColor = 'rgba(246, 126, 126,1)';
 
-export const textDefaultColor = '#2d4150';
+export const textDefaultColor = Colors.dark10;
 export const textColor = '#43515c';
 export const textLinkColor = '#00adf5';
 export const textSecondaryColor = '#7a92a5';
