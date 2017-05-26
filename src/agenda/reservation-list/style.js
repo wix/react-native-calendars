29824ea1a956e42391a0d136d0eafd8bb520985e
georgetroughton <george@ultimatebigfish.net>
2017-05-26 13:49:52 +0100

    implement theme via styleConstructor

--- src/agenda/reservation-list/style.js
+++ src/agenda/reservation-list/style.js
@@ -1,30 +1,32 @@
 import {StyleSheet} from 'react-native';
-import * as defaultStyle from '../style';
+import * as defaultStyle from '../../style';
 
-export default StyleSheet.create({
-  container: {
-    flexDirection: 'row'
-  },
-  dayNum: {
-    fontSize: 28,
-    fontWeight: '200',
-    color: defaultStyle.reservationListDayNumColor
-  },
-  dayText: {
-    fontSize: 14,
-    fontWeight: '300',
-    color: defaultStyle.reservationListDayTextColor,
-    marginTop: -5,
-    backgroundColor: 'rgba(0,0,0,0)'
-  },
-  day: {
-    width: 63,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginTop: 32
-  },
-  today: {
-    color: defaultStyle.reservationListTodayColor
-  }
-});
-
+export default function styleConstructor(theme = {}) {
+  const appStyle = {...defaultStyle, ...theme};
+  return  StyleSheet.create({
+    container: {
+      flexDirection: 'row'
+    },
+    dayNum: {
+      fontSize: 28,
+      fontWeight: '200',
+      color: appStyle.reservationListDayNumColor
+    },
+    dayText: {
+      fontSize: 14,
+      fontWeight: '300',
+      color: appStyle.reservationListDayTextColor,
+      marginTop: -5,
+      backgroundColor: 'rgba(0,0,0,0)'
+    },
+    day: {
+      width: 63,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      marginTop: 32
+    },
+    today: {
+      color: appStyle.reservationListTodayColor
+    }
+  });
+}
