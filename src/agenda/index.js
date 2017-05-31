6fde6afddf9d8c53f37b73cd2353eacb2db93b2b
Johnny <johnny.iday@gmail.com>
2017-05-31 21:07:31 +0800

    Update index.js

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -182,7 +182,7 @@ export default class AgendaView extends Component {
             scrollingEnabled={this.state.calendarScrollable}
             hideExtraDays={this.state.calendarScrollable}
             firstDay={this.props.firstDay}
-            ...this.props
+            {...this.props}
           />
           {knob}
         </Animated.View>
