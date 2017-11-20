164b170ffaf7d9d5293db41f86b5315717856eb7
Albin Ekblom <albinekblom@gmail.com>
2017-11-20 12:55:27 +0100

    🎉 Add showWeekNumbers

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -16,6 +16,7 @@ class CalendarHeader extends Component {
     firstDay: PropTypes.number,
     renderArrow: PropTypes.func,
     hideDayNames: PropTypes.bool,
+    weekNumbers: PropTypes.bool
   };
 
   constructor(props) {
@@ -94,6 +95,7 @@ class CalendarHeader extends Component {
         {
           !this.props.hideDayNames &&
           <View style={this.style.week}>
+            {this.props.weekNumbers && <Text style={this.style.dayHeader}></Text>}
             {weekDaysNames.map((day, idx) => (
               <Text key={idx} style={this.style.dayHeader} numberOfLines={1}>{day}</Text>
             ))}
