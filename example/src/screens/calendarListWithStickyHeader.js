9f0af889444db9ff87c6bcb383dd071b92d2a124
Rida Sarwar <rida.sarwar@ImranPiyars-Mac-mini.local>
2018-01-24 14:30:36 +0500

    You can add more list views with this awesome calendar sticky header

--- example/src/screens/calendarListWithStickyHeader.js
+++ example/src/screens/calendarListWithStickyHeader.js
@@ -4,7 +4,6 @@ import {
   StyleSheet,
   ScrollView,
   View,
-  TouchableOpacity
 } from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
@@ -18,13 +17,8 @@ export default class calendarListWithStickyHeader extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <TouchableOpacity
+      <View
       style={{ height:80 }}
-      onPress={() => {
-        this.setState({
-          calendarStatus: !this.state.calendarStatus
-        });
-      }}
     >
       <Calendar
       theme={{
@@ -40,39 +34,13 @@ export default class calendarListWithStickyHeader extends Component {
         selectedDotColor: "red",
         arrowColor: "#fff",
         monthTextColor: "#fff",
-        // textDayFontFamily: Fonts.type.medium,
-        // textMonthFontFamily: Fonts.type.heavy,
-        // textDayHeaderFontFamily: Fonts.type.medium,
         textDayFontSize: 15,
         textMonthFontSize: 15,
         textDayHeaderFontSize: 15
       }}
-      // showWeekNumbers={false}
-      // Handler which gets executed on day press. Default = undefined
       onDayPress={day => {
         console.log("selected day", day);
       }}
-      // onMonthChange={month => {
-      //   console.log("month changed", month);
-      // }}
-      // markedDates={{
-      //   "2018-01-20": { textColor: "green" },
-      //   "2018-02-22": { startingDay: true, color: "green" },
-      //   "2018-03-23": {
-      //     selected: true,
-      //     endingDay: true,
-      //     color: "green",
-      //     textColor: "gray"
-      //   },
-      //   "2018-01-04": {
-      //     disabled: true,
-      //     startingDay: true,
-      //     color: "green",
-      //     endingDay: true
-      //   }
-      // }}
-      // Date marking style [simple/period/multi-dot]. Default = 'simple'
-      // markingType={"period"}
       dayComponent={({ date, state }) => {
         this.setState({ date: date.dateString });
         return (
@@ -82,7 +50,7 @@ export default class calendarListWithStickyHeader extends Component {
         );
       }}
     />
-    </TouchableOpacity>
+    </View>
     <ScrollView>
     
 <Text style={styles.text}>Weekend Menu</Text>
@@ -107,17 +75,17 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     paddingTop: 5,
     borderBottomWidth: 1,
-    borderColor: '#eee',
+    borderColor: 'green',
     height: 350
   },
   text: {
     textAlign: 'center',
-    borderColor: '#bbb',
+    borderColor: 'red',
     padding: 10,
-    backgroundColor: '#eee'
+    backgroundColor: '#fff'
   },
   container: {
     flex: 1,
-    backgroundColor: 'gray'
+    backgroundColor: '#fff'
   }
 });
