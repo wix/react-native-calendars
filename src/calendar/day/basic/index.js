2a71cfdfcdc1394324624ed8aeaee9bd829c81c4
nehrasajal <nehra.sajal@gmail.com>
2018-02-01 03:41:50 +0530

    added 'selectedColor' property
    Added 'selectedColor' property to change selected day 'backgroundColor'.

--- src/calendar/day/basic/index.js
+++ src/calendar/day/basic/index.js
@@ -80,6 +80,9 @@ class Day extends Component {
 
     if (marking.selected) {
       containerStyle.push(this.style.selected);
+      if (marking.selectedColor) {
+        containerStyle.push({backgroundColor: marking.selectedColor});
+      }
       dotStyle.push(this.style.selectedDot);
       textStyle.push(this.style.selectedText);
     } else if (typeof marking.disabled !== 'undefined' ? marking.disabled : this.props.state === 'disabled') {
