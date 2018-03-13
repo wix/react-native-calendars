6e35a5e9c568645eadb76c63139c284ec6d3facd
Mika Andrianarijaona <mikaoelitiana@users.noreply.github.com>
2018-03-13 12:00:26 +0100

    fix: enhance calendar month title accessibility
    add accessibilityTraits='header'

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -108,7 +108,7 @@ class CalendarHeader extends Component {
         <View style={this.style.header}>
           {leftArrow}
           <View style={{ flexDirection: 'row' }}>
-            <Text allowFontScaling={false} style={this.style.monthText}>
+            <Text allowFontScaling={false} style={this.style.monthText} accessibilityTraits='header'>
               {this.props.month.toString(this.props.monthFormat ? this.props.monthFormat : 'MMMM yyyy')}
             </Text>
             {indicator}
