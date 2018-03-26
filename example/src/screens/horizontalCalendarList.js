2214a474b6fe6e23e84a04f344173dcbeb582126
Tigran <tigrangit@gmail.com>
2018-03-26 16:02:55 +0200

    Example and ReadMe update

---
+++ example/src/screens/horizontalCalendarList.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import {CalendarList} from '../../react-native-calendars';
+
+export default class HorizontalCalendarList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <CalendarList
+        current={'2012-05-16'}
+        pastScrollRange={24}
+        futureScrollRange={24}
+        horizontal
+        pagingEnabled
+      />
+    );
+  }
+}
