80b55a87165390d889edb5d9e56d414ed03003ce
= <=>
2018-04-04 12:21:39 +0800

    removed renderDay; passed previousItem to renderItem prop, Reservation component;

--- src/agenda/reservation-list/reservation.js
+++ src/agenda/reservation-list/reservation.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {xdateToData} from '../../interface';
-import XDate from 'xdate';
-import dateutils from '../../dateutils';
+import {View} from 'react-native';
 import styleConstructor from './style';
 
 class ReservationListItem extends Component {
@@ -31,40 +28,19 @@ class ReservationListItem extends Component {
     return changed;
   }
 
-  renderDate(date, item) {
-    if (this.props.renderDay) {
-      return this.props.renderDay(date ? xdateToData(date) : undefined, item);
-    }
-    const today = dateutils.sameDate(date, XDate()) ? this.styles.today : undefined;
-    if (date) {
-      return (
-        <View style={this.styles.day}>
-          <Text allowFontScaling={false} style={[this.styles.dayNum, today]}>{date.getDate()}</Text>
-          <Text allowFontScaling={false} style={[this.styles.dayText, today]}>{XDate.locales[XDate.defaultLocale].dayNamesShort[date.getDay()]}</Text>
-        </View>
-      );
-    } else {
-      return (
-        <View style={this.styles.day}/>
-      );
-    }
-  }
-
   render() {
     const {reservation, date} = this.props.item;
+    const {previousItem} = this.props;
+    const previousReservation = previousItem? previousItem.reservation : undefined;
     let content;
     if (reservation) {
-      const firstItem = date ? true : false;
-      content = this.props.renderItem(reservation, firstItem);
+      content = this.props.renderItem(reservation, previousReservation);
     } else {
       content = this.props.renderEmptyDate(date);
     }
     return (
       <View style={this.styles.container}>
-        {this.renderDate(date, reservation)}
-        <View style={{flex:1}}>
-          {content}
-        </View>
+        {content}
       </View>
     );
   }
