a37705439dba961198d4f7a5e40dc3be8765e9c0
Maxence Harm <max@aggedon.com>
2018-04-15 19:33:20 +0200

    update example project

--- example/src/screens/calendars.js
+++ example/src/screens/calendars.js
@@ -96,6 +96,35 @@ export default class CalendarsScreen extends Component {
           }}
           hideArrows={false}
         />
+        <Text style={styles.text}>Calendar with multi-period marking</Text>
+        <Calendar
+          style={styles.calendar}
+          current={'2012-05-16'}
+          markingType={'multi-period'}
+          markedDates={{  
+            '2012-05-16': {  
+              periods: [  
+                { startingDay: true, endingDay: false, color: '#5f9ea0' },
+                { startingDay: true, endingDay: false, color: '#ffa500' },
+              ]
+            },
+            '2012-05-17': {  
+              periods: [  
+                { startingDay: false, endingDay: true, color: '#5f9ea0' },
+                { startingDay: false, endingDay: true, color: '#ffa500' },
+                { startingDay: true, endingDay: false, color: '#f0e68c' },
+              ]
+            },
+            '2012-05-18': {  
+              periods: [  
+                { startingDay: true, endingDay: true, color: '#ffa500' },
+                { color: 'transparent' },
+                { startingDay: false, endingDay: false, color: '#f0e68c' },
+              ]
+            },
+          }}
+          hideArrows={false}
+        />
         <Text style={styles.text}>Calendar with week numbers</Text>
         <Calendar
           onDayPress={this.onDayPress}
