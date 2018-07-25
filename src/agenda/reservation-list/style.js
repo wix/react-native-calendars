83739084382aeeb08529dc4362d20d5f22298974
Reid Weber <reid@meshstudio.io>
2018-07-25 11:33:20 -0700

    Updated Styles

--- src/agenda/reservation-list/style.js
+++ src/agenda/reservation-list/style.js
@@ -12,12 +12,14 @@ export default function styleConstructor(theme = {}) {
     dayNum: {
       fontSize: 28,
       fontWeight: '200',
+      fontFamily: appStyle.textMonthFontFamily,
       color: appStyle.agendaDayNumColor
     },
     dayText: {
       fontSize: 14,
       fontWeight: '300',
       color: appStyle.agendaDayTextColor,
+      fontFamily: appStyle.textDayFontFamily,
       marginTop: -5,
       backgroundColor: 'rgba(0,0,0,0)'
     },
--- src/style.js
+++ src/style.js
@@ -1,21 +1,21 @@
 import {Platform} from 'react-native';
 
 export const foregroundColor = '#ffffff';
-export const backgroundColor = '#f4f4f4';
+export const backgroundColor = '#ffffff';
 export const separatorColor = '#e8e9ec';
 
 export const processedColor = '#a7e0a3';
 export const processingColor = '#ffce5c';
 export const failedColor = 'rgba(246, 126, 126,1)';
 
-export const textDefaultColor = '#2d4150';
-export const textColor = '#43515c';
+export const textDefaultColor = '#485A67';
+export const textColor = '#485A67';
 export const textLinkColor = '#00adf5';
 export const textSecondaryColor = '#7a92a5';
 
-export const textDayFontFamily = 'System';
-export const textMonthFontFamily = 'System';
-export const textDayHeaderFontFamily = 'System';
+export const textDayFontFamily = 'WhitneyHTF-Book';
+export const textMonthFontFamily = 'WhitneyHTF-SemiBold';
+export const textDayHeaderFontFamily = 'WhitneyHTF-Book';
 
 export const textMonthFontWeight = '300';
 
@@ -25,17 +25,17 @@ export const textDayHeaderFontSize = 13;
 
 export const calendarBackground = foregroundColor;
 export const textSectionTitleColor = '#b6c1cd';
-export const selectedDayBackgroundColor = textLinkColor;
+export const selectedDayBackgroundColor = '#123A68';
 export const selectedDayTextColor = foregroundColor;
 export const todayBackgroundColor = undefined;
 export const todayTextColor = textLinkColor;
 export const dayTextColor = textDefaultColor;
 export const textDisabledColor = '#d9e1e8';
-export const dotColor = textLinkColor;
+export const dotColor = '#123A68';
 export const selectedDotColor = foregroundColor;
 export const arrowColor = textLinkColor;
 export const monthTextColor = textDefaultColor;
-export const agendaDayTextColor = '#7a92a5';
-export const agendaDayNumColor = '#7a92a5';
+export const agendaDayTextColor = '#485A67';
+export const agendaDayNumColor = '#485A67';
 export const agendaTodayColor = textLinkColor;
 export const agendaKnobColor = Platform.OS === 'ios' ? '#f2F4f5' : '#4ac4f7';
