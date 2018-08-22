af2b4d80089e83c5d69588a7ee495dda3d2fe88d
eladgel <elad@honeybook.com>
2018-08-22 16:51:23 +0300

    Update style.js

--- src/calendar/day/multi-dot/style.js
+++ src/calendar/day/multi-dot/style.js
@@ -15,7 +15,7 @@ export default function styleConstructor(theme={}) {
       marginTop: 4,
       fontSize: appStyle.textDayFontSize,
       fontFamily: appStyle.textDayFontFamily,
-      fontWeight: appstyle.textDayFontWeight || '300',
+      fontWeight: appStyle.textDayFontWeight || '300',
       color: appStyle.dayTextColor,
       backgroundColor: 'rgba(255, 255, 255, 0)'
     },
