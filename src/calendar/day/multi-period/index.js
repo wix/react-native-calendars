32b735a1980f66cd618c8ab175e5b8bb393818c4
ariverak <jrivera.v@outlook.com>
2018-10-09 16:46:04 -0300

    longPress event added to the "multi-period" day component

--- src/calendar/day/multi-period/index.js
+++ src/calendar/day/multi-period/index.js
@@ -21,10 +21,15 @@ class Day extends Component {
     super(props);
     this.style = styleConstructor(props.theme);
     this.onDayPress = this.onDayPress.bind(this);
+    this.onDayLongPress = this.onDayLongPress.bind(this);
   }
 
   onDayPress() {
     this.props.onPress(this.props.date);
+  }
+
+  onDayLongPress() {
+    this.props.onLongPress(this.props.date);
   }
 
   shouldComponentUpdate(nextProps) {
@@ -92,7 +97,11 @@ class Day extends Component {
         style={{
           alignSelf: 'stretch'
         }}>
-        <TouchableOpacity style={containerStyle} onPress={this.onDayPress}>
+        <TouchableOpacity 
+        style={containerStyle} 
+        onPress={this.onDayPress}
+        onLongPress={this.onDayLongPress}
+        >
           <Text allowFontScaling={false} style={textStyle}>
             {String(this.props.children)}
           </Text>
