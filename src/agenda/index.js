bd4d7f75ef55c51d0fc9db470f0d2cd01018e623
IronTony <zimzum4@gmail.com>
2019-03-16 15:13:13 +0100

    Added missed calendarHeight props to Agenda

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -415,6 +415,7 @@ export default class AgendaView extends Component {
               disabledByDefault={this.props.disabledByDefault}
               displayLoadingIndicator={this.props.displayLoadingIndicator}
               showWeekNumbers={this.props.showWeekNumbers}
+              calendarHeight={this.props.calendarHeight}
             />
           </Animated.View>
           {knob}
