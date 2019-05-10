c65c916e75dd48341fa19f3fec0f2e519b357980
Felipe  Saruhashi <felipesaruhashi@MBP-de-Felipe.domdmz.einstein.br>
2019-05-10 15:43:50 -0300

    fix quote lint issue

--- src/agenda/reservation-list/reservation.js
+++ src/agenda/reservation-list/reservation.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { xdateToData } from "../../interface";
-import XDate from "xdate";
-import dateutils from "../../dateutils";
-import styleConstructor from "./style";
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {xdateToData} from '../../interface';
+import XDate from 'xdate';
+import dateutils from '../../dateutils';
+import styleConstructor from './style';
 
 class ReservationListItem extends Component {
   constructor(props) {
@@ -35,27 +35,23 @@ class ReservationListItem extends Component {
     if (this.props.renderDay) {
       return this.props.renderDay(date ? xdateToData(date) : undefined, item);
     }
-    const today = dateutils.sameDate(date, XDate())
-      ? this.styles.today
-      : undefined;
+    const today = dateutils.sameDate(date, XDate()) ? this.styles.today : undefined;
     if (date) {
       return (
         <View style={this.styles.day}>
-          <Text allowFontScaling={false} style={[this.styles.dayNum, today]}>
-            {date.getDate()}
-          </Text>
-          <Text allowFontScaling={false} style={[this.styles.dayText, today]}>
-            {XDate.locales[XDate.defaultLocale].dayNamesShort[date.getDay()]}
-          </Text>
+          <Text allowFontScaling={false} style={[this.styles.dayNum, today]}>{date.getDate()}</Text>
+          <Text allowFontScaling={false} style={[this.styles.dayText, today]}>{XDate.locales[XDate.defaultLocale].dayNamesShort[date.getDay()]}</Text>
         </View>
       );
     } else {
-      return <View style={this.styles.day} />;
+      return (
+        <View style={this.styles.day}/>
+      );
     }
   }
 
   render() {
-    const { reservation, date } = this.props.item;
+    const {reservation, date} = this.props.item;
     const { showDate } = this.props;
     let content;
     if (reservation) {
@@ -67,7 +63,9 @@ class ReservationListItem extends Component {
     return (
       <View style={this.styles.container}>
         {showDate && this.renderDate(date, reservation)}
-        <View style={{ flex: 1 }}>{content}</View>
+        <View style={{flex:1}}>
+          {content}
+        </View>
       </View>
     );
   }
