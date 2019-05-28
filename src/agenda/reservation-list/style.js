4e3ff8af6ab3fd2c6937f7233d8d56d081bf2921
abimaelandrade <abimaelc.andrade@gmail.com>
2019-05-28 17:26:48 -0300

    ReservationListItem - Styled flex

--- src/agenda/reservation-list/style.js
+++ src/agenda/reservation-list/style.js
@@ -1,30 +1,31 @@
-import {StyleSheet} from 'react-native';
-import * as defaultStyle from '../../style';
+import { StyleSheet } from "react-native";
+import * as defaultStyle from "../../style";
 
-const STYLESHEET_ID = 'stylesheet.agenda.list';
+const STYLESHEET_ID = "stylesheet.agenda.list";
 
 export default function styleConstructor(theme = {}) {
-  const appStyle = {...defaultStyle, ...theme};
-  return  StyleSheet.create({
+  const appStyle = { ...defaultStyle, ...theme };
+  return StyleSheet.create({
     container: {
-      flexDirection: 'row'
+      flex: 1,
+      flexDirection: "row"
     },
     dayNum: {
       fontSize: 28,
-      fontWeight: '200',
+      fontWeight: "200",
       color: appStyle.agendaDayNumColor
     },
     dayText: {
       fontSize: 14,
-      fontWeight: '300',
+      fontWeight: "300",
       color: appStyle.agendaDayTextColor,
       marginTop: -5,
-      backgroundColor: 'rgba(0,0,0,0)'
+      backgroundColor: "rgba(0,0,0,0)"
     },
     day: {
       width: 63,
-      alignItems: 'center',
-      justifyContent: 'flex-start',
+      alignItems: "center",
+      justifyContent: "flex-start",
       marginTop: 32
     },
     today: {
