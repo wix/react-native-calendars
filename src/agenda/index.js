f54b08958841e790006ea745b34029a3d934af0e
hiroya iizuka <39001773+hiroyaiizuka@users.noreply.github.com>
2019-08-01 16:24:35 +0900

    Update index.js

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -268,7 +268,7 @@ export default class AgendaView extends Component {
     }
 
     this.setScrollPadPosition(this.initialScrollPadPosition(), true);
-    this.calendar.scrollToDay(day, this.calendarOffset(), false);
+   // this.calendar.scrollToDay(day, this.calendarOffset(), true);
     
     if (this.props.loadItemsForMonth) {
       this.props.loadItemsForMonth(xdateToData(day));
