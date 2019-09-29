b3c6ade6a5908ba114efa3115cf52b174f441e24
André Coelho <41406021+avrcoelho@users.noreply.github.com>
2019-09-28 22:40:31 -0300

    refactor life cycle deprecated

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -207,7 +207,7 @@ export default class AgendaView extends Component {
     }
   }
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this._isMounted = true;
     this.loadReservations(this.props);
   }
@@ -216,13 +216,15 @@ export default class AgendaView extends Component {
     this._isMounted = false;
   }
 
-  componentWillReceiveProps(props) {
-    if (props.items) {
-      this.setState({
-        firstResevationLoad: false
-      });
-    } else {
-      this.loadReservations(props);
+  componentDidUpdate(props) {
+    if(this.props !== props) {
+      if (props.items) {
+        this.setState({
+          firstResevationLoad: false
+        });
+      } else {
+        this.loadReservations(props);
+      }
     }
   }
 
