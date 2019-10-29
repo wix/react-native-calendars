1ba85f61b7e77b8dbd7ff58cab0b18771b9f05bf
yamato-payforward <githgetI2retaiahsaiko3u676ub@gmail.com>
2019-10-29 19:11:48 +0900

    fix typo #raruhu

--- src/calendar/day/multi-period/index.js
+++ src/calendar/day/multi-period/index.js
@@ -1,4 +1,4 @@
-    import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { TouchableOpacity, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { shouldUpdate } from '../../../component-updater';
@@ -6,6 +6,7 @@ import { shouldUpdate } from '../../../component-updater';
 import styleConstructor from './style';
 
 class Day extends Component {
+  static displayName = 'IGNORE';
   static propTypes = {
     // TODO: disabled props should be removed
     state: PropTypes.oneOf(['disabled', 'today', '']),
