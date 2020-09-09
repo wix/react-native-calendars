b580266d32766086a4076fc52facb59749876294
kais <kaissaroj@gmail.com>
2020-09-09 22:40:05 +0545

    Update index.js
    scrollpad error

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -478,7 +478,7 @@ export default class AgendaView extends Component {
             { useNativeDriver: true },
           )}
         >
-          <View style={{height: agendaHeight + KNOB_HEIGHT}} onLayout={this.onScrollPadLayout} />
+     
         </Animated.ScrollView>
       </View>
     );
