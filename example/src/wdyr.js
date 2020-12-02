a2471f6a91bd80478e1326d4688d7cf4a8a5aa5e
Inbal Tish <inbalti@wix.com>
2020-12-02 09:38:04 +0200

    another options edit

--- example/src/wdyr.js
+++ example/src/wdyr.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
+// NOTE: You must add static `whyDidYouRender = true` to the component you want to track.
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   const options = {
-    // logOnDifferentValues: true
+    logOnDifferentValues: false
   };
 
   whyDidYouRender(React, options);
 }
-
-// NOTE: You must add static `whyDidYouRender = true` to the component you want to track.
