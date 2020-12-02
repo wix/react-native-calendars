e5ca1777bcb4be1f3b8fac3ae468b59ab434ea26
Inbal Tish <inbalti@wix.com>
2020-12-02 09:28:27 +0200

    editing wdyr options

--- example/src/wdyr.js
+++ example/src/wdyr.js
@@ -3,6 +3,8 @@ import React from 'react';
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true,
+    logOnDifferentValues: true,
   });
 }
+
+// NOTE: You must add static `whyDidYouRender = true` to the component you want to track.
