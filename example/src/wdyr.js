4675e380f18795b94f14aea62ba46fc0d82b8e9b
Inbal Tish <inbalti@wix.com>
2020-12-02 09:31:08 +0200

    removing log option

--- example/src/wdyr.js
+++ example/src/wdyr.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    logOnDifferentValues: true,
-  });
+  const options = {
+    // logOnDifferentValues: true
+  };
+
+  whyDidYouRender(React, options);
 }
 
 // NOTE: You must add static `whyDidYouRender = true` to the component you want to track.
