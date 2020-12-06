4814a9b18e1099cd0fe26add490c939f8ba57070
Inbal Tish <inbalti@wix.com>
2020-12-06 11:25:55 +0200

    fix lint errors

--- src/calendar/day/marking/style.js
+++ src/calendar/day/marking/style.js
@@ -1,10 +1,10 @@
 import {StyleSheet} from 'react-native';
-import * as defaultStyle from '../../style';
+// import * as defaultStyle from '../../style';
 
 const STYLESHEET_ID = 'stylesheet.marking';
 
 export default function styleConstructor(theme={}) {
-  const appStyle = {...defaultStyle, ...theme};
+  // const appStyle = {...defaultStyle, ...theme};
   return StyleSheet.create({
     dotContainer: {
       flexDirection: 'row'
