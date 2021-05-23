7274b3b97c2f5e3425c6fa3f9357ca7b8b1433bc
Mendy <mendye@wix.com>
2021-05-23 12:24:19 +0300

    interface spec formatting

--- src/interface.spec.js
+++ src/interface.spec.js
@@ -54,7 +54,7 @@ describe('calendar interface', () => {
     it('should convert xdate to data', () => {
       const time = 1479772800000;
       const testDate = XDate(time, true);
-      expect((testDate).toISOString()).toEqual('2016-11-22T00:00:00Z');
+      expect(testDate.toISOString()).toEqual('2016-11-22T00:00:00Z');
       const data = iface.xdateToData(testDate);
       expect(data).toEqual({
         year: 2016,
