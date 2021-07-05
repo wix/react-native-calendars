df945a7814de5eab2834563fcab78c8973157056
Ethan Sharabi <ethans@wix.com>
2021-07-05 17:48:36 +0300

    Fix ts-expect-error

--- src/calendar/day/dot/index.tsx
+++ src/calendar/day/dot/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {View} from 'react-native';
 import styleConstructor from './style';
 
-interface DotProps {
+export interface DotProps {
   theme?: Object;
   color?: String;
   marked?: Boolean;
