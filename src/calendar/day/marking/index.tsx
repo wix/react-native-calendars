d7c632c78f378b7344006041d371c91c6119211c
Inbal Tish <inbalti@wix.com>
2021-08-01 13:12:04 +0300

    Marking - type fix

--- src/calendar/day/marking/index.tsx
+++ src/calendar/day/marking/index.tsx
@@ -1,5 +1,8 @@
+import _ from 'lodash';
+
 import React, {Component} from 'react';
 import {View, ViewStyle, TextStyle} from 'react-native';
+
 // @ts-expect-error
 import {shouldUpdate, extractComponentProps} from '../../../component-updater';
 import styleConstructor from './style';
@@ -21,14 +24,14 @@ type CustomStyle = {
 
 type DOT = {
   key?: string;
-  color?: string;
+  color: string;
   selectedDotColor?: string;
 };
 
 type PERIOD = {
+  color: string;
   startingDay?: boolean;
   endingDay?: boolean;
-  color?: string;
 };
 
 export interface MarkingProps extends DotProps {
@@ -44,9 +47,9 @@ export interface MarkingProps extends DotProps {
   selectedTextColor?: string;
   dotColor?: string;
   //multi-dot
-  dots?: DOT;
+  dots?: DOT[];
   //multi-period
-  periods?: PERIOD;
+  periods?: PERIOD[];
   startingDay?: boolean;
   endingDay?: boolean;
   accessibilityLabel?: string;
@@ -83,12 +86,12 @@ export default class Marking extends Component<MarkingProps> {
     ]);
   }
 
-  getItems(items: DOT | PERIOD) {
+  getItems(items?: DOT[] | PERIOD[]) {
     const {type} = this.props;
 
     if (items && Array.isArray(items) && items.length > 0) {
       // Filter out items so that we process only those which have color property
-      const validItems = items.filter(d => d && d.color);
+      const validItems = _.filter(items, function(o: DOT | PERIOD) { return !o.color; });
 
       return validItems.map((item, index) => {
         return type === MarkingTypes.MULTI_DOT ? this.renderDot(index, item) : this.renderPeriod(index, item);
@@ -108,7 +111,7 @@ export default class Marking extends Component<MarkingProps> {
     }
   }
 
-  renderMultiMarkings(containerStyle: Object, items: any) {
+  renderMultiMarkings(containerStyle: Object, items?: DOT[] | PERIOD[]) {
     return <View style={containerStyle}>{this.getItems(items)}</View>;
   }
 
@@ -126,7 +129,7 @@ export default class Marking extends Component<MarkingProps> {
     if (endingDay) {
       style.push(this.style.endingDay);
     }
-    return <View key={index} style={style} />;
+    return <View key={index} style={style}/>;
   }
 
   renderDot(index?: number, item?: any) {
