2a525ecc93ca3de1fbe27067d6a13da022d41da2
Fraser Benjamin <fraser.benjamin@outlook.com>
2021-09-02 13:44:22 +0100

    Fix usage condition

--- src/agenda/reservation-list/index.tsx
+++ src/agenda/reservation-list/index.tsx
@@ -123,7 +123,7 @@ class ReservationList extends Component<ReservationListProps, ReservationsListSt
     this.heights = [];
     this.selectedDay = props.selectedDay;
     this.scrollOver = true;
-    this.scrollBetweenWeeks = props.scrollBetweenWeeks ? props.scrollBetweenWeeks : true;
+    this.scrollBetweenWeeks = props.scrollBetweenWeeks === false ? false : true;
   }
 
   componentDidMount() {
