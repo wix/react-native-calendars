f81bec26a0ebeb6cf53092e9021a2ade9d7fc349
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2021-12-30 13:08:12 +0200

    fix script

---
+++ demo/docsRNC/docs/components/Calendar.md
@@ -0,0 +1,18 @@
+Calendar component  
+[(code example)](https://github.com/wix/react-native-calendars/blob/master/example/src/screens/calendars.tsx)
+
+<div style={{display: 'flex', flexDirection: 'row', overflowX: 'auto', maxHeight: '500px', alignItems: 'center'}}><img style={{maxHeight: '420px'}} src={'../../demo/assets/calendar.gif'}/>
+
+</div>
+
+## API
+
+### theme
+
+Specify theme properties to override specific styles for calendar parts  
+<span style={{color: 'grey'}}>Theme</span>
+
+### style
+
+Specify style for calendar container element  
+<span style={{color: 'grey'}}>ViewStyle</span>
--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,9 +1,9 @@
 const childProcess = require('child_process');
 const fs = require('fs');
 
-const COMPONENTS_DOCS_DIR = '../demo/docsRNC/docs/components';
+const COMPONENTS_DOCS_DIR = './demo/docsRNC/docs/components';
 
-const result = childProcess.execSync('find ./src -name "api.json"');
+const result = childProcess.execSync('find ./src -name "*api.json"');
 const apiFiles = result.toString().trim().split('\n');
 
 const components = apiFiles.map(filePath => {
@@ -16,30 +16,28 @@ if (!fs.existsSync(COMPONENTS_DOCS_DIR)) {
   fs.mkdirSync(COMPONENTS_DOCS_DIR);
 }
 
-components.forEach(component => {
-  /* General */
-  let content = `${component.description}  \n`;
-  content += `[(code example)](${component.example})\n`;
+if (components) {
+  components.forEach(component => {
+    /* General */
+    let content = `${component.description}  \n`;
+    content += `[(code example)](${component.example})\n`;
 
-  /* Images */
-  content += `<div style={{display: 'flex', flexDirection: 'row', overflowX: 'auto', maxHeight: '500px', alignItems: 'center'}}>`;
-  component.images?.forEach(image => {
-    content += `<img style={{maxHeight: '420px'}} src={'${image}'}/>`;
-    content += '\n\n';
+    /* Images */
+    content += `<div style={{display: 'flex', flexDirection: 'row', overflowX: 'auto', maxHeight: '500px', alignItems: 'center'}}>`;
+    component.images?.forEach(image => {
+      content += `<img style={{maxHeight: '420px'}} src={'${image}'}/>`;
+      content += '\n\n';
+    });
+    content += '</div>\n\n';
+
+    /* Props */
+    content += `## API\n`;
+    component.props.forEach(prop => {
+      content += `### ${prop.name}\n`;
+      content += `${prop.description}  \n`;
+      content += `<span style={{color: 'grey'}}>${prop.type}</span>\n\n`;
+    });
+
+    fs.writeFileSync(`${COMPONENTS_DOCS_DIR}/${component.name}.md`, content, {encoding: 'utf8'});
   });
-  content += '</div>\n\n';
-
-  /* Props */
-  content += `## API\n`;
-  component.props.forEach(prop => {
-    content += `### ${prop.name}\n`;
-    content += `${prop.description}  \n`;
-    content += `<span style={{color: 'grey'}}>${prop.type}</span>\n\n`;
-  });
-
-  if (!fs.existsSync(`${COMPONENTS_DOCS_DIR}/${component.category}`)) {
-    fs.mkdirSync(`${COMPONENTS_DOCS_DIR}/${component.category}`);
-  }
-
-  fs.writeFileSync(`${COMPONENTS_DOCS_DIR}/${component.category}/${component.name}.md`, content, {encoding: 'utf8'});
-});
+}
