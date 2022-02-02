cd982bf244f0f443a8b6ad0114afca90c2b62683
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-02-02 08:53:41 +0200

    replacing logo; changing color palette, deleting unused files

--- docsRNC/src/pages/index.js
+++ docsRNC/src/pages/index.js
@@ -21,7 +21,7 @@ function HomepageHeader() {
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Site main title <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
