19a657441bcd05dd5979a49d05266b9a6bfbe4dc
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-03-08 07:37:46 +0200

    Agenda - fix for 'selected' as undefined

--- src/agenda/index.tsx
+++ src/agenda/index.tsx
@@ -114,8 +114,8 @@ export default class Agenda extends Component<AgendaProps, State> {
       calendarIsReady: false,
       calendarScrollable: false,
       firstReservationLoad: false,
-      selectedDay: parseDate(props.selected) || new XDate(true),
-      topDay: parseDate(props.selected) || new XDate(true)
+      selectedDay: this.getSelectedDate(),
+      topDay: this.getSelectedDate()
     };
 
     this.currentMonth = this.state.selectedDay.clone();
@@ -134,9 +134,9 @@ export default class Agenda extends Component<AgendaProps, State> {
     this.state.scrollY.removeAllListeners();
   }
 
-  componentDidUpdate(prevProps: AgendaProps) {
-    if (!sameDate(parseDate(this.props.selected), parseDate(prevProps.selected))) {
-      this.setState({selectedDay: parseDate(this.props.selected)});
+  componentDidUpdate(prevProps: AgendaProps, prevState: State) {
+    if (this.props.selected && !sameDate(new XDate(this.props.selected), prevState.selectedDay)) {
+      this.setState({selectedDay: this.getSelectedDate()});
     } else if (!prevProps.items) {
       this.loadReservations(this.props);
     }
@@ -147,6 +147,11 @@ export default class Agenda extends Component<AgendaProps, State> {
       return {firstReservationLoad: false};
     }
     return null;
+  }
+
+  getSelectedDate() {
+    const {selected} = this.props;
+    return selected ? parseDate(selected) : new XDate(true);
   }
 
   calendarOffset() {
