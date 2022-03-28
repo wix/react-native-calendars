d36de1c88b6f45ea144297a0052f48db88eecf7b
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-03-28 08:55:42 +0300

    rename 'calendarProvider'

--- src/expandableCalendar/Context/Presenter.ts
+++ src/expandableCalendar/Context/Presenter.ts
@@ -4,7 +4,7 @@ import {sameMonth, isToday, isPastDate} from '../../dateutils';
 import {xdateToData, toMarkingFormat} from '../../interface';
 import {getDefaultLocale} from '../../services';
 import {UpdateSources} from '../commons';
-import {CalendarContextProviderProps} from './contextProvider';
+import {CalendarContextProviderProps} from './calendarProvider';
 
 const commons = require('../commons');
 const TOP_POSITION = 65;
