71177fb3d206c409777a335aebb7bb42c29a4e1c
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-07-07 09:53:47 +0300

    fix master merge. move to function

--- example/src/screens/expandableCalendarScreen.tsx
+++ example/src/screens/expandableCalendarScreen.tsx
@@ -1,237 +1,93 @@
-import React, {Component} from 'react';
+import React, {useRef, useCallback} from 'react';
 import {StyleSheet} from 'react-native';
 import {ExpandableCalendar, AgendaList, CalendarProvider, WeekCalendar} from 'react-native-calendars';
 import testIDs from '../testIDs';
-
-const today = new Date().toISOString().split('T')[0];
-const fastDate = getPastDate(3);
-const futureDates = getFutureDates(9);
-const dates = [fastDate, today].concat(futureDates);
-const themeColor = '#00AAAF';
-const lightThemeColor = '#EBF9F9';
-
-function getFutureDates(numberOfDays: number) {
-  const array = [];
-  for (let index = 1; index <= numberOfDays; index++) {
-    const date = new Date(Date.now() + 864e5 * index); // 864e5 == 86400000 == 24*60*60*1000
-    const dateString = date.toISOString().split('T')[0];
-    array.push(dateString);
-  }
-  return array;
-}
-
-function getPastDate(numberOfDays: number) {
-  return new Date(Date.now() - 864e5 * numberOfDays).toISOString().split('T')[0];
-}
-
-const ITEMS: any[] = [
-  {
-    title: dates[0],
-    data: [{hour: '12am', duration: '1h', title: 'First Yoga'}]
-  },
-  {
-    title: dates[1],
-    data: [
-      {hour: '4pm', duration: '1h', title: 'Pilates ABC'},
-      {hour: '5pm', duration: '1h', title: 'Vinyasa Yoga'}
-    ]
-  },
-  {
-    title: dates[2],
-    data: [
-      {hour: '1pm', duration: '1h', title: 'Ashtanga Yoga'},
-      {hour: '2pm', duration: '1h', title: 'Deep Stretches'},
-      {hour: '3pm', duration: '1h', title: 'Private Yoga'}
-    ]
-  },
-  {
-    title: dates[3],
-    data: [{hour: '12am', duration: '1h', title: 'Ashtanga Yoga'}]
-  },
-  {
-    title: dates[4],
-    data: [{}]
-  },
-  {
-    title: dates[5],
-    data: [
-      {hour: '9pm', duration: '1h', title: 'Middle Yoga'},
-      {hour: '10pm', duration: '1h', title: 'Ashtanga'},
-      {hour: '11pm', duration: '1h', title: 'TRX'},
-      {hour: '12pm', duration: '1h', title: 'Running Group'}
-    ]
-  },
-  {
-    title: dates[6], 
-    data: [
-      {hour: '12am', duration: '1h', title: 'Ashtanga Yoga'}
-    ]
-  },
-  {
-    title: dates[7], 
-    data: [{}]
-  },
-  {
-    title: dates[8],
-    data: [
-      {hour: '9pm', duration: '1h', title: 'Pilates Reformer'},
-      {hour: '10pm', duration: '1h', title: 'Ashtanga'},
-      {hour: '11pm', duration: '1h', title: 'TRX'},
-      {hour: '12pm', duration: '1h', title: 'Running Group'}
-    ]
-  },
-  {
-    title: dates[9],
-    data: [
-      {hour: '1pm', duration: '1h', title: 'Ashtanga Yoga'},
-      {hour: '2pm', duration: '1h', title: 'Deep Stretches'},
-      {hour: '3pm', duration: '1h', title: 'Private Yoga'}
-    ]
-  },
-  {
-    title: dates[10], 
-    data: [
-      {hour: '12am', duration: '1h', title: 'Last Yoga'}
-    ]
-  }
-];
-
-type MarkedDates = {
-  [key: string]: object;
-}
-
-function getMarkedDates(items: any[]) {
-  const marked: MarkedDates = {};
-
-  items.forEach(item => {
-    // NOTE: only mark dates with data
-    if (item.data && item.data.length > 0 && !isEmpty(item.data[0])) {
-      marked[item.title] = {marked: true};
-    } else {
-      marked[item.title] = {disabled: true};
-    }
-  });
-  return marked;
-}
-
-function getTheme() {
-  const disabledColor = 'grey';
-
-  return {
-    // arrows
-    arrowColor: 'black',
-    arrowStyle: {padding: 0},
-    // knob
-    expandableKnobColor: themeColor,
-    // month
-    monthTextColor: 'black',
-    textMonthFontSize: 16,
-    textMonthFontFamily: 'HelveticaNeue',
-    textMonthFontWeight: 'bold' as 'bold',
-    // day names
-    textSectionTitleColor: 'black',
-    textDayHeaderFontSize: 12,
-    textDayHeaderFontFamily: 'HelveticaNeue',
-    textDayHeaderFontWeight: 'normal' as 'normal',
-    // dates
-    dayTextColor: themeColor,
-    textDayFontSize: 18,
-    textDayFontFamily: 'HelveticaNeue',
-    textDayFontWeight: '500' as '500',
-    textDayStyle: {marginTop: Platform.OS === 'android' ? 2 : 4},
-    // selected date
-    selectedDayBackgroundColor: themeColor,
-    selectedDayTextColor: 'white',
-    // disabled date
-    textDisabledColor: disabledColor,
-    // dot (marked date)
-    dotColor: themeColor,
-    selectedDotColor: 'white',
-    disabledDotColor: disabledColor,
-    dotStyle: {marginTop: -2}
-  };
-}
+import {agendaItems, getMarkedDates} from '../mocks/agendaItems';
+import AgendaItem from '../mocks/AgendaItem';
+import {getTheme, themeColor, lightThemeColor} from '../mocks/theme';
 
 const leftArrowIcon = require('../img/previous.png');
 const rightArrowIcon = require('../img/next.png');
+const ITEMS: any[] = agendaItems;
 
 interface Props {
   weekView?: boolean;
 }
 
-export default class ExpandableCalendarScreen extends Component<Props> {
-  marked = getMarkedDates();
-  theme = getTheme();
-  todayBtnTheme = {
+const ExpandableCalendarScreen = (props: Props) => {
+  const {weekView} = props;
+  const marked = useRef(getMarkedDates());
+  const theme = useRef(getTheme());
+  const todayBtnTheme = useRef({
     todayButtonTextColor: themeColor
-  };
+  });
 
-  onDateChanged = (/* date, updateSource */) => {
+  const onDateChanged = useCallback((/* date, updateSource */) => {
     // console.warn('ExpandableCalendarScreen onDateChanged: ', date, updateSource);
     // fetch and set data for date + week ahead
-  };
+  }, []);
 
-  onMonthChange = (/* month, updateSource */) => {
+  const onMonthChange = useCallback((/* month, updateSource */) => {
     // console.warn('ExpandableCalendarScreen onMonthChange: ', month, updateSource);
-  };
+  }, []);
 
-  renderItem = ({item}: any) => {
+  const renderItem = useCallback(({item}: any) => {
     return <AgendaItem item={item}/>;
-  };
+  }, []);
 
-  render() {
-    return (
-      <CalendarProvider
-        date={ITEMS[1].title}
-        onDateChanged={this.onDateChanged}
-        onMonthChange={this.onMonthChange}
-        showTodayButton
-        disabledOpacity={0.6}
-        theme={this.todayBtnTheme}
-        // todayBottomMargin={16}
-      >
-        {this.props.weekView ? (
-          <WeekCalendar testID={testIDs.weekCalendar.CONTAINER} firstDay={1} markedDates={this.marked}/>
-        ) : (
-          <ExpandableCalendar
-            testID={testIDs.expandableCalendar.CONTAINER}
-            // horizontal={false}
-            // hideArrows
-            // disablePan
-            // hideKnob
-            // initialPosition={ExpandableCalendar.positions.OPEN}
-            // calendarStyle={styles.calendar}
-            // headerStyle={styles.calendar} // for horizontal only
-            // disableWeekScroll
-            theme={this.theme}
-            // disableAllTouchEventsForDisabledDays
-            firstDay={1}
-            markedDates={this.marked}
-            leftArrowImageSource={leftArrowIcon}
-            rightArrowImageSource={rightArrowIcon}
-            // animateScroll
-          />
-        )}
-        <AgendaList
-          sections={ITEMS}
-          renderItem={this.renderItem}
-          // scrollToNextEvent
-          // sectionStyle={styles.section}
-          // dayFormat={'YYYY-MM-d'}
+  return (
+    <CalendarProvider
+      date={ITEMS[1].title}
+      onDateChanged={onDateChanged}
+      onMonthChange={onMonthChange}
+      showTodayButton
+      disabledOpacity={0.6}
+      theme={todayBtnTheme.current}
+      // todayBottomMargin={16}
+    >
+      {weekView ? (
+        <WeekCalendar testID={testIDs.weekCalendar.CONTAINER} firstDay={1} markedDates={marked.current}/>
+      ) : (
+        <ExpandableCalendar
+          testID={testIDs.expandableCalendar.CONTAINER}
+          // horizontal={false}
+          // hideArrows
+          // disablePan
+          // hideKnob
+          // initialPosition={ExpandableCalendar.positions.OPEN}
+          // calendarStyle={styles.calendar}
+          // headerStyle={styles.calendar} // for horizontal only
+          // disableWeekScroll
+          theme={theme.current}
+          // disableAllTouchEventsForDisabledDays
+          firstDay={1}
+          markedDates={marked.current}
+          leftArrowImageSource={leftArrowIcon}
+          rightArrowImageSource={rightArrowIcon}
+          // animateScroll
         />
-      </CalendarProvider>
-    );
-  }
-}
+      )}
+      <AgendaList
+        sections={ITEMS}
+        renderItem={renderItem}
+        // scrollToNextEvent
+        // sectionStyle={styles.section}
+        // dayFormat={'YYYY-MM-d'}
+      />
+    </CalendarProvider>
+  );
+};
 
-const styles = StyleSheet.create({
-  calendar: {
-    paddingLeft: 20,
-    paddingRight: 20
-  },
-  section: {
-    backgroundColor: lightThemeColor,
-    color: 'grey',
-    textTransform: 'capitalize'
-  }
-});
+export default ExpandableCalendarScreen;
+
+// const styles = StyleSheet.create({
+//   calendar: {
+//     paddingLeft: 20,
+//     paddingRight: 20
+//   },
+//   section: {
+//     backgroundColor: lightThemeColor,
+//     color: 'grey',
+//     textTransform: 'capitalize'
+//   }
+// });
