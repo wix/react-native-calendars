0c80057f1381087f3474375eacb4ce03195c18e3
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-07-12 11:44:54 +0300

    Move `setDate` and `setDisabled` back to provider and removing presenter's tests

--- src/expandableCalendar/Context/Provider.tsx
+++ src/expandableCalendar/Context/Provider.tsx
@@ -1,17 +1,15 @@
-import PropTypes from 'prop-types';
 import XDate from 'xdate';
 
 import React, {useEffect, useRef, useState, useCallback, useMemo} from 'react';
 import {Animated, TouchableOpacity, View, ViewStyle, ViewProps, StyleProp} from 'react-native';
 
-import {toMarkingFormat} from '../../interface';
+import {sameMonth} from '../../dateutils';
+import {xdateToData} from '../../interface';
 import {Theme, DateData} from '../../types';
-import styleConstructor from '../style';
 import {UpdateSources} from '../commons';
+import styleConstructor from '../style';
 import CalendarContext from './index';
 import {
-  setDate,
-  setDisabled,
   shouldAnimateTodayButton,
   shouldAnimateOpacity,
   getButtonIcon,
@@ -24,12 +22,12 @@ import {
 const TOP_POSITION = 65;
 
 export interface CalendarContextProviderProps extends ViewProps {
+  /** Initial date in 'yyyy-MM-dd' format. Default = now */
+  date: string; //TODO: rename 'initialDate'
   /** Specify theme properties to override specific styles for calendar parts */
   theme?: Theme;
   /** Specify style for calendar container element */
   style?: StyleProp<ViewStyle>;
-  /** Initial date in 'yyyy-MM-dd' format. Default = now */
-  date: string; //TODO: rename 'initialDate'
   /** Callback for date change event */
   onDateChanged?: (date: string, updateSource: UpdateSources) => void;
   /** Callback for month change event */
@@ -65,19 +63,18 @@ const CalendarProvider = (props: CalendarContextProviderProps) => {
     children
   } = props;
   const style = useRef(styleConstructor(theme));
+  const buttonY = useRef(new Animated.Value(todayBottomMargin ? -todayBottomMargin : -TOP_POSITION));
+  const opacity = useRef(new Animated.Value(1));
+  const today = useRef(getTodayFormatted());
+  const wrapperStyle = useMemo(() => {
+    return [style.current.contextWrapper, propsStyle];
+  }, [style, propsStyle]);
 
   const [prevDate, setPrevDate] = useState(date);
   const [currentDate, setCurrentDate] = useState(date);
   const [updateSource, setUpdateSource] = useState(UpdateSources.CALENDAR_INIT);
   const [isDisabled, setIsDisabled] = useState(false);
-  const [buttonIcon, setButtonIcon] = useState(getButtonIcon(date || toMarkingFormat(new XDate()), showTodayButton));
-  
-  const buttonY = useRef(new Animated.Value(todayBottomMargin ? -todayBottomMargin : -TOP_POSITION));
-  const opacity = useRef(new Animated.Value(1));
-
-  const wrapperStyle = useMemo(() => {
-    return [style.current.contextWrapper, propsStyle];
-  }, [style, propsStyle]);
+  const [buttonIcon, setButtonIcon] = useState(getButtonIcon(date, showTodayButton));
 
   useEffect(() => {
     if (date) {
@@ -89,24 +86,27 @@ const CalendarProvider = (props: CalendarContextProviderProps) => {
     animateTodayButton(currentDate);
   }, [todayBottomMargin, currentDate]);
 
-  const _setDate = (date: string, updateSource: UpdateSources) => {
-    const updateState = (buttonIcon: number) => {
-      setPrevDate(currentDate);
-      setCurrentDate(date);
-      setUpdateSource(updateSource);
-      setButtonIcon(buttonIcon);
-    };
+  /** Context */
 
-    setDate(props, date, currentDate, updateState, updateSource);
+  const _setDate = (date: string, updateSource: UpdateSources) => {
+    setPrevDate(currentDate);
+    setCurrentDate(date);
+    setUpdateSource(updateSource);
+    setButtonIcon(getButtonIcon(date, showTodayButton));
+
+    props.onDateChanged?.(date, updateSource);
+
+    if (!sameMonth(new XDate(date), new XDate(currentDate))) {
+      props.onMonthChange?.(xdateToData(new XDate(date)), updateSource);
+    }
   };
 
   const _setDisabled = (disabled: boolean) => {
-    const updateState = (disabled: boolean) => {
-      setIsDisabled(disabled);
-      animateOpacity(disabled);
-    };
-
-    setDisabled(showTodayButton, disabled, isDisabled, updateState);
+    if (!showTodayButton || disabled === isDisabled) {
+      return;
+    }
+    setIsDisabled(disabled);
+    animateOpacity(disabled);
   };
 
   const contextValue = useMemo(() => {
@@ -120,6 +120,8 @@ const CalendarProvider = (props: CalendarContextProviderProps) => {
       timelineLeftInset
     };
   }, [currentDate, prevDate, updateSource, numberOfDays]);
+
+  /** Animations */
 
   const animateTodayButton = (date: string) => {
     if (shouldAnimateTodayButton(props)) {
@@ -141,13 +143,15 @@ const CalendarProvider = (props: CalendarContextProviderProps) => {
     }
   };
 
+  /** Events */
+
   const onTodayPress = useCallback(() => {
     _setDate(getTodayDate(), UpdateSources.TODAY_PRESS);
   }, [_setDate]);
 
-  const renderTodayButton = () => {
-    const today = getTodayFormatted();
+  /** Renders */
 
+  const renderTodayButton = () => {
     return (
       <Animated.View style={[style.current.todayButtonContainer, {transform: [{translateY: buttonY.current}]}]}>
         <TouchableOpacity
@@ -157,7 +161,7 @@ const CalendarProvider = (props: CalendarContextProviderProps) => {
         >
           <Animated.Image style={[style.current.todayButtonImage, {opacity: opacity.current}]} source={buttonIcon}/>
           <Animated.Text allowFontScaling={false} style={[style.current.todayButtonText, {opacity: opacity.current}]}>
-            {today}
+            {today.current}
           </Animated.Text>
         </TouchableOpacity>
       </Animated.View>
@@ -175,14 +179,3 @@ const CalendarProvider = (props: CalendarContextProviderProps) => {
 export default CalendarProvider;
 
 CalendarProvider.displayName = 'CalendarProvider';
-CalendarProvider.propTypes = {
-  date: PropTypes.any.isRequired,
-  onDateChanged: PropTypes.func,
-  onMonthChange: PropTypes.func,
-  showTodayButton: PropTypes.bool,
-  todayBottomMargin: PropTypes.number,
-  todayButtonStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.number, PropTypes.array]),
-  disabledOpacity: PropTypes.number,
-  numberOfDays: PropTypes.number,
-  timelineLeftInset: PropTypes.number
-};
