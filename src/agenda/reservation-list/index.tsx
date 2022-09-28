89bcf5bc159777ccf614ed545ac76a8605a621d9
Katya Kostikova <70844218+katyakostikova@users.noreply.github.com>
2022-09-28 14:16:42 +0300

    disabled onDaySelected scroll in reservations list when showOnlySelectedDayItems (#1911)
    Co-authored-by: katyakostikova <“kostikova21@gmail.com”>

--- src/agenda/reservation-list/index.tsx
+++ src/agenda/reservation-list/index.tsx
@@ -123,10 +123,10 @@ class ReservationList extends Component<ReservationListProps, State> {
   }
 
   updateReservations(props: ReservationListProps) {
-    const {selectedDay} = props;
+    const {selectedDay, showOnlySelectedDayItems} = props;
     const reservations = this.getReservations(props);
     
-    if (this.list && !sameDate(selectedDay, this.selectedDay)) {
+    if (!showOnlySelectedDayItems && this.list && !sameDate(selectedDay, this.selectedDay)) {
       let scrollPosition = 0;
       for (let i = 0; i < reservations.scrollPosition; i++) {
         scrollPosition += this.heights[i] || 0;
