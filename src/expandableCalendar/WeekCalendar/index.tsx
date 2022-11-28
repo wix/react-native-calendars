9ccaf67ec5b18c99b2489239311214bbf2fa74af
yuvalsho <49347352+yuvalsho@users.noreply.github.com>
2022-11-28 12:34:16 +0200

    add integration tests on expandable calendar (#2081)
    * add integration tests on expandable calendar
    
    * PR fix
    
    * PR fix

--- src/expandableCalendar/WeekCalendar/index.tsx
+++ src/expandableCalendar/WeekCalendar/index.tsx
@@ -16,7 +16,7 @@ import {extractCalendarProps} from '../../componentUpdater';
 import CalendarContext from '../Context';
 import {useDidUpdate} from '../../hooks';
 
-const NUMBER_OF_PAGES = 6;
+export const NUMBER_OF_PAGES = 6;
 const NUM_OF_ITEMS = NUMBER_OF_PAGES * 2 + 1; // NUMBER_OF_PAGES before + NUMBER_OF_PAGES after + current
 const APPLY_ANDROID_FIX = constants.isAndroid && constants.isRTL;
 
