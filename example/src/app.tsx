f9c6dc62c178b26830b24a86eecb2e3a37f9ef1d
hanseulhee <3021062@gmail.com>
2024-06-27 19:04:45 +0900

    feat: add LocaleConfig locales Korea

--- example/src/app.tsx
+++ example/src/app.tsx
@@ -1,9 +1,9 @@
+import { Navigation } from 'react-native-navigation';
 import './wdyr'; // <--- must be first import
-import {Navigation} from 'react-native-navigation';
 // import {I18nManager} from 'react-native'; // <--- In order to test RTL
 //@ts-expect-error
-import {LocaleConfig} from 'react-native-calendars';
-import {registerScreens} from './screens';
+import { LocaleConfig } from 'react-native-calendars';
+import { registerScreens } from './screens';
 
 // I18nManager.forceRTL(true); // <--- In order to test RTL
 registerScreens();
@@ -32,6 +32,27 @@ LocaleConfig.locales['en'] = {
   // numbers: ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] // number localization example
 };
 LocaleConfig.defaultLocale = 'en';
+
+LocaleConfig.locales['ko'] = {
+  monthNames: [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월',
+  ],
+  monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+  dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
+  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+  // numbers: ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] // number localization example
+};
 
 /*
 LocaleConfig.locales['fr'] = {
