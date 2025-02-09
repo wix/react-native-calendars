e7b4b46f551f969506963b97142722d4af970777
Nitzan Yizhar <nitzany@wix.com>
2025-02-09 11:47:20 +0200

    Refactor useHeaderHeight to use inferred type for headerHeight state

--- src/expandableCalendar/useHeaderHeight.ts
+++ src/expandableCalendar/useHeaderHeight.ts
@@ -2,7 +2,7 @@ import {useCallback, useState} from 'react';
 import type {LayoutChangeEvent} from 'react-native';
 
 const useHeaderHeight = () => {
-    const [headerHeight, setHeaderHeight] = useState<number>(0);
+    const [headerHeight, setHeaderHeight] = useState(0);
     const onHeaderLayout = useCallback(({nativeEvent: {layout: {height}}}: LayoutChangeEvent) => {
         setHeaderHeight(height);
     }, []);
