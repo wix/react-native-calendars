3bd5417fa16dbf6f0bfd5fa600ae51b9b9de60ec
Inbal Tish <inbalti@wix.com>
2025-02-25 10:07:32 +0200

    additional fixes

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        root: ['.'],
+        alias: {
+          'react-native-calendars': './src/index.ts',
+          commons: './src/commons',
+          services: './src/services',
+          utils: './src/utils',
+          hooks: './src/hooks.ts',
+          style: './src/style.ts'
+        }
+      }
+    ]
+  ]
 };
--- example/src/app.tsx
+++ example/src/app.tsx
@@ -4,10 +4,19 @@ import {AppRegistry} from 'react-native';
 import {createStaticNavigation} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 // import {I18nManager} from 'react-native'; // <--- In order to test RTL
-// import {LocaleConfig} from 'react-native-calendars';
+//@ts-expect-error
+import {LocaleConfig} from 'react-native-calendars';
 import {name as appName} from '../app.json';
 import MenuScreen from './screens/menuScreen';
-// import CalendarScreen from './screens/calendarScreen';
+import CalendarScreen from './screens/calendarScreen';
+import CalendarPlaygroundScreen from './screens/calendarPlaygroundScreen';
+import AgendaScreen from './screens/agendaScreen';
+import AgendaInfiniteListScreen from './screens/agendaInfiniteListScreen';
+import CalendarsListScreen from './screens/calendarListScreen';
+import NewCalendarsListScreen from './screens/newCalendarListScreen';
+import ExpandableCalendarScreen from './screens/expandableCalendarScreen';
+import TimelineCalendarScreen from './screens/timelineCalendarScreen';
+import PlaygroundScreen from './screens/playgroundScreen';
 
 AppRegistry.registerComponent(appName, () => App);
 
@@ -15,58 +24,58 @@ AppRegistry.registerComponent(appName, () => App);
 // eslint-disable-next-line no-console
 console.ignoredYellowBox = ['Remote debugger'];
 
-// LocaleConfig.locales['en'] = {
-//   formatAccessibilityLabel: "dddd d 'of' MMMM 'of' yyyy",
-//   monthNames: [
-//     'January',
-//     'February',
-//     'March',
-//     'April',
-//     'May',
-//     'June',
-//     'July',
-//     'August',
-//     'September',
-//     'October',
-//     'November',
-//     'December'
-//   ],
-//   monthNamesShort: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
-//   dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-//   dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-//   // numbers: ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] // <--- number localization example
-// };
+LocaleConfig.locales['en'] = {
+  formatAccessibilityLabel: "dddd d 'of' MMMM 'of' yyyy",
+  monthNames: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ],
+  monthNamesShort: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+  dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+  dayNamesShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+  // numbers: ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] // <--- number localization example
+};
 
-// LocaleConfig.locales['fr'] = {
-//   monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-//   monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
-//   dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
-//   dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.'],
-//   today: 'Aujourd\'hui'
-// };
+LocaleConfig.locales['fr'] = {
+  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
+  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
+  dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
+  dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.'],
+  today: 'Aujourd\'hui'
+};
 
-// LocaleConfig.locales['he'] = {
-//   formatAccessibilityLabel: "dddd d 'of' MMMM 'of' yyyy",
-//   monthNames: [
-//     'ינואר',
-//     'פברואר',
-//     'מרץ',
-//     'אפריל',
-//     'מאי',
-//     'יוני',
-//     'יולי',
-//     'אוגוסט',
-//     'ספטמבר',
-//     'אוקטובר',
-//     'נובמבר',
-//     'דצמבר'
-//   ],
-//   monthNamesShort: ['ינו', 'פבר', 'מרץ', 'אפר', 'מאי', 'יונ', 'יול', 'אוג', 'ספט', 'אוק', 'נוב', 'דצמ'],
-//   dayNames: ['ראון', 'שני', 'שלישי', 'קביעי', 'חמישי', 'שישי', 'שבת'],
-//   dayNamesShort: ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש'],
-// };
+LocaleConfig.locales['he'] = {
+  formatAccessibilityLabel: "dddd d 'of' MMMM 'of' yyyy",
+  monthNames: [
+    'ינואר',
+    'פברואר',
+    'מרץ',
+    'אפריל',
+    'מאי',
+    'יוני',
+    'יולי',
+    'אוגוסט',
+    'ספטמבר',
+    'אוקטובר',
+    'נובמבר',
+    'דצמבר'
+  ],
+  monthNamesShort: ['ינו', 'פבר', 'מרץ', 'אפר', 'מאי', 'יונ', 'יול', 'אוג', 'ספט', 'אוק', 'נוב', 'דצמ'],
+  dayNames: ['ראון', 'שני', 'שלישי', 'קביעי', 'חמישי', 'שישי', 'שבת'],
+  dayNamesShort: ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ש'],
+};
 
-// LocaleConfig.defaultLocale = 'en';
+LocaleConfig.defaultLocale = 'en';
 
 /** React Navigation */
 
@@ -75,7 +84,15 @@ const RootStack = createNativeStackNavigator({
     Home: {
       screen: MenuScreen
     },
-    // Calendar: CalendarScreen
+    Calendar: CalendarScreen,
+    CalendarPlayground: CalendarPlaygroundScreen,
+    Agenda: AgendaScreen,
+    AgendaInfiniteList: AgendaInfiniteListScreen,
+    CalendarList: CalendarsListScreen,
+    NewCalendarList: NewCalendarsListScreen,
+    ExpandableCalendar: ExpandableCalendarScreen,
+    TimelineCalendar: TimelineCalendarScreen,
+    Playground: PlaygroundScreen
   },
 });
 const Navigation = createStaticNavigation(RootStack);
