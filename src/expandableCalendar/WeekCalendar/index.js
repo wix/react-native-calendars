826d40713ab3cf9f931ce13000fd26f534b932a9
Eleanor Green <eleanorgreen@eqt-hub.com>
2025-03-13 12:49:35 +0000

    fix: remove inconsistent date spacing in WeekCalendar

--- src/expandableCalendar/WeekCalendar/index.js
+++ src/expandableCalendar/WeekCalendar/index.js
@@ -9,8 +9,5 @@ function getDate(date, firstDay, weekIndex, numberOfDays) {
     d.addDays(firstDay - dayOfTheWeek);
     // Add the appropriate number of weeks
     const newDate = numberOfDays && numberOfDays > 1 ? d.addDays(weekIndex * numberOfDays) : d.addWeeks(weekIndex);
-    const today = new XDate();
-    const offsetFromNow = newDate.diffDays(today);
-    const isSameWeek = offsetFromNow > 0 && offsetFromNow < (numberOfDays ?? 7);
-    return toMarkingFormat(isSameWeek ? today : newDate);
+    return toMarkingFormat(newDate);
 } 
