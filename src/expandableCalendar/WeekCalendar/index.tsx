df845e05b07b49004e8b9d23ef49c1f5610f3f29
David Alford <davidalford678@gmail.com>
2025-05-21 12:12:46 -0700

    Fix: ensure unique keys in WeekCalendar FlatList to prevent React warnings

--- src/expandableCalendar/WeekCalendar/index.tsx
+++ src/expandableCalendar/WeekCalendar/index.tsx
@@ -133,7 +133,7 @@ const WeekCalendar = (props: WeekCalendarProps) => {
     );
   },[firstDay, _onDayPress, context, date, markedDates]);
 
-  const keyExtractor = useCallback((item, index) => `key-${item.date}-${index}`, []);
+  const keyExtractor = useCallback((item, index) => `${item}-${index}`, []);
 
   const renderWeekDaysNames = useMemo(() => {
     return (
