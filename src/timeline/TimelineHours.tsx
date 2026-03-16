2f2b4c94acc9921c5b2d35ac71ce3ca6ffcc31da
Jens Kuhr Hansen <jenskuhr4@gmail.com>
2026-03-16 21:00:12 +0100

    Migrate StyleSheet.absoluteFillObject
    Migrate StyleSheet.absoluteFillObject which has been removed in react-native 0.85: https://github.com/facebook/react-native/releases/tag/v0.85.0-rc.0

--- src/timeline/TimelineHours.tsx
+++ src/timeline/TimelineHours.tsx
@@ -100,7 +100,7 @@ const TimelineHours = (props: TimelineHoursProps) => {
   return (
     <>
       <TouchableWithoutFeedback onLongPress={handleBackgroundPress} onPressOut={handlePressOut}>
-        <View style={StyleSheet.absoluteFillObject}/>
+        <View style={StyleSheet.absoluteFill}/>
       </TouchableWithoutFeedback>
       {unavailableHoursBlocks.map((block, index) => (
         <View
